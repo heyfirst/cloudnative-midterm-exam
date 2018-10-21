a382e74c7d7c4e3db6f3f3d9cf6152d841e1e595
Kanisorn S <firstziiz.k@gmail.com>
2018-10-21 16:00:06 +0700

    Update findAllElectiveCourse to get enrolls

--- sit-ecr-elective-course-service/src/main/java/sit/cloudnative/sitecrelectivecourseservice/ElectiveCourse/ElectiveCourseService.java
+++ sit-ecr-elective-course-service/src/main/java/sit/cloudnative/sitecrelectivecourseservice/ElectiveCourse/ElectiveCourseService.java
@@ -2,6 +2,7 @@ package sit.cloudnative.sitecrelectivecourseservice.ElectiveCourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sit.cloudnative.sitecrelectivecourseservice.Enroll.EnrollService;
 
 import java.util.List;
 
@@ -11,8 +12,17 @@ public class ElectiveCourseService {
     @Autowired
     private ElectiveCourseRepository electiveCourseRepository;
 
+    @Autowired
+    private EnrollService enrollService;
+
     public List findAllElectiveCourse() {
         List electiveCourses = electiveCourseRepository.findAll();
+        for(int i = 0; i < electiveCourses.size(); i++) {
+            ElectiveCourse electiveCourse = (ElectiveCourse) electiveCourses.get(i);
+            List enrolls = enrollService.findEnrollsByElectiveCourseId(electiveCourse.getId());
+            electiveCourse.setEnrolls(enrolls);
+            System.out.println(electiveCourse.getEnrolls());
+        }
         return electiveCourses;
     }
 
