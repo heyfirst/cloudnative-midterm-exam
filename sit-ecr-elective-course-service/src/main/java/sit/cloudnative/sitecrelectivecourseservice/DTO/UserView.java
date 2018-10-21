7be094e8210eebf16eebd06a6ce91eabf7dcc088
Kanisorn S <firstziiz.k@gmail.com>
2018-10-21 17:58:41 +0700

    Create DTO Class for User

---
+++ sit-ecr-elective-course-service/src/main/java/sit/cloudnative/sitecrelectivecourseservice/DTO/UserView.java
@@ -0,0 +1,85 @@
+package sit.cloudnative.sitecrelectivecourseservice.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.LocalDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserView {
+
+    private Long id;
+    private String  studentId;
+    private String  name;
+    private String  surname;
+    private String  department;
+    private String  faculty;
+    private Integer year;
+    private LocalDateTime created;
+
+    public UserView() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+}
