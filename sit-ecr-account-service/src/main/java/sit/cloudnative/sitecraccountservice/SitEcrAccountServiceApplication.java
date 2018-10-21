169a6074b5a0f3ea24230f89c283b33854319b70
Kanisorn S <firstziiz.k@gmail.com>
2018-10-21 16:49:08 +0700

    Initial Accounts Service

---
+++ sit-ecr-account-service/src/main/java/sit/cloudnative/sitecraccountservice/SitEcrAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package sit.cloudnative.sitecraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitEcrAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitEcrAccountServiceApplication.class, args);
+	}
+}
