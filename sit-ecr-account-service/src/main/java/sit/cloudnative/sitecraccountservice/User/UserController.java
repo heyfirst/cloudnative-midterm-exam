44ef954146f6a97f98c8afb201be33e4d525e8d2
Kanisorn S <firstziiz.k@gmail.com>
2018-10-21 16:49:45 +0700

    Add UserController and Service

---
+++ sit-ecr-account-service/src/main/java/sit/cloudnative/sitecraccountservice/User/UserController.java
@@ -0,0 +1,39 @@
+package sit.cloudnative.sitecraccountservice.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user/")
+    public ResponseEntity<User> post(@RequestBody User newEnroll) {
+        User user = userService.createUser(newEnroll);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> findByID(@PathVariable Long id) {
+        User user = userService.findUserByID(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List> findAll() {
+        List users = userService.findAllUser();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> put(@PathVariable Long id, @RequestBody Object input) {
+        return null;
+    }
+}
---
+++ sit-ecr-account-service/src/main/java/sit/cloudnative/sitecraccountservice/User/UserService.java
@@ -0,0 +1,29 @@
+package sit.cloudnative.sitecraccountservice.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List findAllUser() {
+        List enrolls = userRepository.findAll();
+        return enrolls;
+    }
+
+    public User findUserByID(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException());
+        return user;
+    }
+
+    public User createUser(User newRegister) {
+        User user = userRepository.save(newRegister);
+        return user;
+    }
+}
