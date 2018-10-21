6315363b79dee08efc1dd6a86ed7a9e371dff9bb
Kanisorn S <firstziiz.k@gmail.com>
2018-10-21 18:00:57 +0700

    Update createEnroll

--- sit-ecr-elective-course-service/src/main/java/sit/cloudnative/sitecrelectivecourseservice/Enroll/EnrollController.java
+++ sit-ecr-elective-course-service/src/main/java/sit/cloudnative/sitecrelectivecourseservice/Enroll/EnrollController.java
@@ -15,14 +15,32 @@ public class EnrollController {
 
     @PostMapping("/enroll/")
     public ResponseEntity<Enroll> post(@RequestBody Enroll newEnroll) {
+
+        Integer userId = newEnroll.getUserId();
+
+        if (!enrollService.isUserCanEnroll(userId)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        if (enrollService.isOverEnrollLimitByUserId(userId)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
         Enroll enroll = enrollService.createEnroll(newEnroll);
         return new ResponseEntity<>(enroll, HttpStatus.OK);
+
     }
 
     @GetMapping("/enroll/{id}")
     public ResponseEntity<Enroll> findByID(@PathVariable Integer id) {
         Enroll enroll = enrollService.findEnrollByID(id);
         return new ResponseEntity<>(enroll, HttpStatus.OK);
+    }
+
+    @GetMapping("/enrolls/user-id/{id}")
+    public ResponseEntity<List> findByUserID(@PathVariable Integer id) {
+        List enrolls = enrollService.findEnrollsByUserId(id);
+        return new ResponseEntity<>(enrolls, HttpStatus.OK);
     }
 
     @GetMapping("/enrolls")
